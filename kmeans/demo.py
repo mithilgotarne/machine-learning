17c4652c360457f456f4e393e08aa940da5c24d9
mithil <mithilgotarne@gmail.com>
2018-02-27 13:09:45 +0530

    added output

--- kmeans/demo.py
+++ kmeans/demo.py
@@ -35,6 +35,7 @@ def kmeans(num_of_clusters, data, num_of_iterations):
             centers = new_centers
         
         print([len(c) for c in clusters])
+        #plot(data, clusters)
     
     return clusters
 
@@ -55,11 +56,22 @@ def plot(data, clusters):
 def main():
     data = np.genfromtxt('data.csv', delimiter=',')
     data = np.delete(data[1:], [0], axis=1)
-    num_of_clusters = 4
+    num_of_clusters = 2
     num_of_iterations = 100
     clusters = kmeans(num_of_clusters, data, num_of_iterations)
     plot(data, clusters)
     
 if __name__ == '__main__':
     main()
-    
+
+"""
+[1054, 2946]
+[800, 3200]
+[[ 179.22   95.      0.  ]
+ [ 192.34   69.      0.  ]
+ [ 140.25   92.      0.  ]
+ ..., 
+ [  46.85    7.      1.  ]
+ [  37.68    7.      1.  ]
+ [  50.56    5.      1.  ]]
+"""    
