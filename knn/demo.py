f3a3737b0a2ca7da4bc626de5af65fc6482f8155
mithil <mithilgotarne@gmail.com>
2018-03-20 00:32:16 +0530

    changes

--- knn/demo.py
+++ knn/demo.py
@@ -1,9 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 
-def euclidian(x,y):
+
+def euclidian(x, y):
     return np.sqrt(np.sum((x-y)**2))
+
 
 def get_class(distances):
     count = {}
@@ -15,11 +16,12 @@ def get_class(distances):
             count[point_class] += 1
     return sorted(count, key=count.get)[-1]
 
+
 if __name__ == '__main__':
     num_of_points = 5
     data = np.genfromtxt('data.csv', delimiter=',')
 
-    data = data [1:,1:]
+    data = data[1:, 1:]
 
     random.shuffle(data)
 
@@ -30,8 +32,8 @@ if __name__ == '__main__':
     correct = 0
 
     for point in test_data:
-        distances = [(euclidian(point[:-1],p[:-1]), p) for p in train_data]
-        distances = sorted(distances,key= lambda x: x[0])[:5]
+        distances = [(euclidian(point[:-1], p[:-1]), p) for p in train_data]
+        distances = sorted(distances, key=lambda x: x[0])[:num_of_points]
         point_class = get_class(distances)
         print("Actual->", point[-1], " Predicted->", point_class)
         if point_class == point[-1]:
@@ -52,6 +54,7 @@ Actual-> 4.0  Predicted-> 4.0
 Actual-> 2.0  Predicted-> 2.0
 Actual-> 4.0  Predicted-> 4.0
 Actual-> 4.0  Predicted-> 4.0
+...
 Actual-> 4.0  Predicted-> 4.0
 Actual-> 2.0  Predicted-> 2.0
 Actual-> 4.0  Predicted-> 4.0
@@ -59,221 +62,6 @@ Actual-> 2.0  Predicted-> 2.0
 Actual-> 2.0  Predicted-> 2.0
 Actual-> 2.0  Predicted-> 2.0
 Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 4.0  Predicted-> 4.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
-Actual-> 2.0  Predicted-> 2.0
 Actual-> 2.0  Predicted-> 2.0
 Actual-> 2.0  Predicted-> 2.0
 Accuracy: 0.9535714285714286
